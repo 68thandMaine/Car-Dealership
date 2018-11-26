fd1b5f0ab40a8c6c82c180fe8beee383359485d8
Student <example@example.com>
2018-11-26 14:06:51 -0800

    Add a form to create a new car object and several get/set methods in the models folder

--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -5,49 +5,63 @@ namespace CarDealership.Models {
 
   class Car
   {
-    private string MakeModel;
+    private string Make;
+    private string Model;
     private int Price;
     private int Miles;
     private string Note;
     private static List<cars> _inventory = new List<cars> {};
 
-    public Car(string makeModel, int price, int miles, string note)
+    public Car(string make, string model, int price, int miles, string note)
     {
-      MakeModel = makeModel;
+      Make = make;
+      Model = model;
       Price = price;
       Miles = miles;
       Note = note;
     }
 
-    public string GetMakeModel()
+    public string GetMake()
     {
-      return MakeModel;
+      return Make;
     }
-
+    public void SetMake(string newMake)
+    {
+      Make = newMake;
+    }
+    public string GetModel()
+    {
+      return Model;
+    }
+    public void SetModel(string newModel)
+    {
+      Model = newModel;
+    }
     public int GetPrice()
     {
       return Price;
     }
-
+    public void SetPrice(int newPrice)
+    {
+      Price = newPrice;
+    }
     public int GetMiles()
     {
       return Miles;
     }
-
+    public void SetMiles(int newMiles)
+    {
+      Miles = newMiles;
+    }
     public string GetNote()
     {
       return Note;
     }
-
     public void SetNote(string newNote)
     {
       Note = newNote;
     }
 
-    public void SetPrice(int newPrice)
-    {
-      Price = newPrice;
-    }
 
     public void Save()
     {
