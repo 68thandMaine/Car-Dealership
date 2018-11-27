98697f63a01b064b0815e27caecb9339436922dd
Student <example@example.com>
2018-11-26 16:35:22 -0800

    Add HttpPost method to the carscontroller file

--- CarDealership.Tests/ControllerTests/CarsControllerTests.cs
+++ CarDealership.Tests/ControllerTests/CarsControllerTests.cs
@@ -3,3 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using CarDealership.Controllers;
 using CarDealership.Models;
+
+namespace CarsControllerTests.Test
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    {
+      CarsController controller = new CarsController();
+      ActionResult view = controller.Create("Honda", "Crv", 14000, 1300, "Great car, but needs a new coat of paint after driving it through an industrial sized sand blaster accidentally");
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+    }
+  }
+}
